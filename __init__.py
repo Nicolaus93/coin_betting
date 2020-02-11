472d2a2fb8a21d084d08432cbbfcb17874296c86
Zhenxun Zhuang <oldboymls@gmail.com>
2020-02-11 13:01:08 -0500

    import adam and sgd from pytorch

--- __init__.py
+++ __init__.py
@@ -9,6 +9,10 @@ Created on Tue Feb 11 12:38:22 2020
 The interface is consistent with the torch.optim package.
 """
 
+from .adam import Adam  # noqa: F401
+from .sgd import SGD  # noqa: F401
 from .optimizer import Optimizer  # noqa: F401
 
+del adam
+del sgd
 del optimizer
