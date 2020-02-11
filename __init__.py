2599d0c9bc96a4aa2240deae50bbf350e0a4998e
Zhenxun Zhuang <oldboymls@gmail.com>
2020-02-11 12:43:36 -0500

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""
+Created on Tue Feb 11 12:38:22 2020
+
+@author: Zhenxun Zhuang
+"""
+
+"""
+:mod:`optimal_pytorch` is a package implementing various optimization algorithms.
+The interface is consistent with the torch.optim package.
+"""
+
+from .optimizer import Optimizer  # noqa: F401
+
+del optimizer
