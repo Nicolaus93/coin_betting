bc05e199ed1b5156d947969a3946b4b5e9809855
nicoloc <nico.campolongo@gmail.com>
2020-07-01 15:32:13 +0200

    adding optimizers to the package

--- optimal_pytorch/__init__.py
+++ optimal_pytorch/__init__.py
@@ -12,10 +12,37 @@ from .adam import Adam  # noqa: F401
 from .sgd import SGD  # noqa: F401
 from .sgdol import SGDOL  # noqa: F401
 from .optimizer import Optimizer  # noqa: F401
+from .accsgd import AccSGD
+from .adabound import AdaBound
+from .adamod import AdaMod
+from .diffgrad import DiffGrad
+from .lamb import Lamb
+from .lookahead import Lookahead
+from .novograd import NovoGrad
+from .pid import PID
+from .radam import RAdam
+from .sgdw import SGDW
+from .yogi import Yogi
 
-del adam
-del sgd
-del sgdol
-del optimizer
 
-__all__ = ['Adam', 'SGD', 'SGDOL', 'Optimizer']
+# del adam
+# del sgd
+# del sgdol
+# del optimizer
+
+__all__ = [
+    'Adam',
+    'SGD',
+    'SGDOL',
+    'Optimizer',
+    'AccSGD',
+    'AdaBound',
+    'AdaMod',
+    'DiffGrad',
+    'Lamb',
+    'Lookahead',
+    'NovoGrad',
+    'PID',
+    'RAdam',
+    'SGDW',
+    'Yogi']
