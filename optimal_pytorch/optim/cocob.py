5e90dd42640063b1f240f482875c83e935952623
nicoloc <nico.campolongo@gmail.com>
2021-04-14 11:59:15 +0200

    modified cocob algorithm

--- optimal_pytorch/optim/cocob.py
+++ optimal_pytorch/optim/cocob.py
@@ -1,64 +1,62 @@
 import torch
-from torch.optim.optimizer import Optimizer
-from .types import OptFloat, OptLossClosure, Params
+from torch.optim import Optimizer
+from utils import convex_f
+from typing import TYPE_CHECKING, Any, Optional, Callable
 
-__all__ = ('Cocob',)
+if TYPE_CHECKING:
+    from torch.optim.optimizer import _params_t
+else:
+    _params_t = Any
 
 
 class Cocob(Optimizer):
     """
-    COntinuos COin Betting (COCOB) optimizer from the paper
-    Training Deep Networks without Learning Rates Through Coin Betting.
+    Implements COntinuos COin Betting (COCOB) algorithm.
+
+    Proposed in "Training Deep Networks without Learning Rates Through Coin Betting",
     https://arxiv.org/abs/1705.07795
+
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups.
+        alpha (float): (hyper)parameter to guarantee that the fraction to bet
+            is at least alpha * L_{t, i} for each weight (default: 100.0).
     """
 
-    def __init__(
-        self,
-        params: Params,
-        alpha: float = 100,
-        eps: float = 1e-8,
-        weight_decay: float = 0,
-    ) -> None:
-        if not 0.0 <= eps:
-            raise ValueError("Invalid epsilon value: {}".format(eps))
+    def __init__(self, params: _params_t, alpha: float = 100.0):
         if not 0.0 <= alpha:
             raise ValueError("Invalid alpha value: {}".format(alpha))
-        if not 0.0 <= weight_decay:
-            raise ValueError(
-                "Invalid weight_decay value: {}".format(weight_decay))
 
-        defaults = dict(
-            alpha=alpha,
-            eps=eps,
-            weight_decay=weight_decay)
-        self._alpha = alpha
-        self._eps = eps
-
+        defaults = dict(alpha=alpha)
         super(Cocob, self).__init__(params, defaults)
 
-    def grid_search_params(self):
-        ranges = {}
-        ranges['alpha'] = [100, 'use']
-        ranges['weight_decay'] = [1e-3, 10, 'gen', 6]
-        return ranges
+        for group in self.param_groups:                 # State initialization
+            for p in group['params']:
+                state = self.state[p]
+                state['w1'] = p.clone().detach()        # initial model
+                state['theta'] = torch.zeros_like(p)    # sum of the gradients
+                state['Gt'] = torch.zeros_like(p)       # sum of abs value of gradients
+                state['Lt'] = torch.full_like(p, 1e-8)  # maximum observed scale
+                state['reward'] = torch.zeros_like(p)   # cumulative reward
 
-    def step(self, closure: OptLossClosure = None) -> OptFloat:
+    @torch.no_grad()
+    def step(self, closure: Optional[Callable[[], float]] = None) -> Optional[float]:
         r"""Performs a single optimization step.
         Arguments:
             closure: A closure that reevaluates the model and returns the loss.
         """
-
         loss = None
         if closure is not None:
             loss = closure()
 
         for group in self.param_groups:
+            alpha = group['alpha']
 
             for p in group['params']:
                 if p.grad is None:
                     continue
 
-                grad = p.grad.data
+                grad = -p.grad.data  # negative gradient
                 if grad.is_sparse:
                     msg = (
                         'Cocob does not support sparse gradients, '
@@ -66,49 +64,20 @@ class Cocob(Optimizer):
                     )
                     raise RuntimeError(msg)
 
+                # Retrieve parameters
                 state = self.state[p]
+                w1 = state['w1']
+                theta = state['theta']
+                Gt = state['Gt']
+                Lt = state['Lt']
+                reward = state['reward']
 
-                # State initialization
-                if len(state) == 0:
-                    # sum of the gradients
-                    state['gradients_sum'] = torch.zeros_like(p)
-                    # sum of the absolute values of the subgradients
-                    state['grad_norm_sum'] = torch.zeros_like(p)
-                    # Update the maximum observed scale
-                    state['L'] = self._eps * torch.ones_like(p)
-                    # tilde_w_t = w_1 - w_t
-                    state['tilde_w'] = torch.zeros_like(p)
-                    # reward
-                    state['reward'] = torch.zeros_like(p)
-
-                gradients_sum, grad_norm_sum, L, tilde_w, reward = (
-                    state['gradients_sum'],
-                    state['grad_norm_sum'],
-                    state['L'],
-                    state['tilde_w'],
-                    state['reward'],
-                )
-
-                if group['weight_decay'] != 0:
-                    grad.add_(p.data, alpha=group['weight_decay'])
-
-                # absolute value of current gradient vector
+                # Update parameters (inplace)
                 abs_grad = torch.abs(grad)
-                # update parameters
-                L = torch.max(L, abs_grad)
-                gradients_sum.add_(grad)
-                grad_norm_sum.add_(abs_grad)
-                reward = torch.max(reward - grad * tilde_w, torch.zeros_like(reward))
-                den = L * torch.max(grad_norm_sum + L, self._alpha * L)
-                x = (gradients_sum / den) * (reward + L)
-                p.data.add_(-tilde_w - x)
-                tilde_w = -x
-
-                # state update
-                state['gradients_sum'] = gradients_sum
-                state['grad_norm_sum'] = grad_norm_sum
-                state['L'] = L
-                state['tilde_w'] = tilde_w
-                state['reward'] = reward
+                torch.max(Lt, abs_grad, out=Lt)
+                theta.add_(grad)
+                Gt.add_(abs_grad)
+                torch.max(reward + (p.data - w1) * grad, torch.zeros_like(reward), out=reward)
+                p.data = w1 + theta / (Lt * (torch.max(Gt + Lt, alpha * Lt))) * (reward + Lt)
 
         return loss
