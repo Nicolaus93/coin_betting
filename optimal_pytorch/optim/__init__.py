3587ed1b4d35882cb0e6cb8339e4ec2bd3c5392e
nicoloc <nico.campolongo@gmail.com>
2021-04-14 12:43:15 +0200

    removing old optimizers

--- optimal_pytorch/optim/__init__.py
+++ optimal_pytorch/optim/__init__.py
@@ -1,52 +1,21 @@
 """
 Created on Tue Feb 11th, 2020
 
-@author: Zhenxun Zhuang
+@authors: Zhenxun Zhuang, Nicolò Campolongo, Vibhu Bhatia
 """
 """
-:mod:`optimal_pytorch` is a package implementing various optimization algorithms.
+:mod:`optimal_pytorch` is a package implementing various coin-betting algorithms.
 The interface is consistent with the torch.optim package.
 """
 
-from .adam import Adam                           # noqa: F401
-from .sgd import SGD                             # noqa: F401
-from .sgdol import SGDOL                         # noqa: F401
-from .optimizer import Optimizer                 # noqa: F401
-from .accsgd import AccSGD                       # noqa: F401
-from .adabound import AdaBound                   # noqa: F401
-from .adamod import AdaMod                       # noqa: F401
-from .diffgrad import DiffGrad                   # noqa: F401
-from .lamb import Lamb                           # noqa: F401
-from .lookahead import Lookahead                 # noqa: F401
-from .novograd import NovoGrad                   # noqa: F401
-from .pid import PID                             # noqa: F401
-from .radam import RAdam                         # noqa: F401
-from .sgdw import SGDW                           # noqa: F401
-from .yogi import Yogi                           # noqa: F401
-from .cocob import Cocob                         # noqa: F401
-from .coin_betting import CoinBetting            # noqa: F401
+from .sgdol import SGDOL              # noqa: F401
+from .cocob import Cocob              # noqa: F401
+from .recursive import Recursive      # noqa: F401
+from .regralizer import Regralizer    # noqa: F401
 
-
-# del adam
-# del sgd
-# del sgdol
-# del optimizer
 
 __all__ = [
-    'Adam',
-    'SGD',
     'SGDOL',
-    'Optimizer',
-    'AccSGD',
-    'AdaBound',
-    'AdaMod',
     'Cocob',
-    'DiffGrad',
-    'Lamb',
-    'Lookahead',
-    'NovoGrad',
-    'PID',
-    'RAdam',
-    'SGDW',
-    'Yogi',
-    'CoinBetting']
+    'Recursive',
+    'Regralizer']
